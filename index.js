a4166dff22fa8caa24a6d826f10441fe7384e160
md-ashik-max <ar1214697@gmail.com>
2024-06-04 08:19:42 +0600

    added post method for post user information in database

--- index.js
+++ index.js
@@ -28,6 +28,23 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+        const userCollection = client.db("contestDB").collection("users");
+
+        // user related api
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const query = { email: user.email }
+            const existingUser = await userCollection.findOne(query)
+            if (existingUser) {
+                return res.send({ message: 'user already exists', insertedId: null })
+            }
+            const result = await userCollection.insertOne(user)
+            res.send(result)
+
+        })
+
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
