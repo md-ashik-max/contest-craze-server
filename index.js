f4b4d0399d7bd25bce905a256d25c04745584c9d
md-ashik-max <ar1214697@gmail.com>
2024-06-04 09:20:41 +0600

    added patch method for update user role

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -31,7 +31,17 @@ async function run() {
 
         const userCollection = client.db("contestDB").collection("users");
 
+
         // user related api
+
+
+        app.get('/users', async (req, res) => {
+            const result = await userCollection.find().toArray();
+            res.send(result)
+        })
+
+
+
         app.post('/users', async (req, res) => {
             const user = req.body;
             const query = { email: user.email }
@@ -42,6 +52,18 @@ async function run() {
             const result = await userCollection.insertOne(user)
             res.send(result)
 
+        })
+
+        app.patch('/users/admin/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) }
+            const updatedDoc = {
+                $set: {
+                    role: 'admin'
+                }
+            }
+            const result = await userCollection.updateOne(filter, updatedDoc)
+            res.send(result)
         })
 
 
