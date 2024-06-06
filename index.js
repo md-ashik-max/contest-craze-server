a7b92a7e9b145ea01b1f32c9db10137b57478244
md-ashik-max <ar1214697@gmail.com>
2024-06-07 02:20:01 +0600

    added stipe payment api

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const stripe = require("stripe")(process.env.STRIPE_PAYMENT_KEY);
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
@@ -191,6 +192,21 @@ async function run() {
             const query = { _id: new ObjectId(id) }
             const result = await contestsCollection.deleteOne(query)
             res.send(result)
+        })
+
+        // payment related api 
+
+        app.post('/create-payment-intent',async(req,res)=>{
+            const{price}=req.body;
+            const amount = parseInt(price*100);
+            const paymentIntent = await stripe.paymentIntents.create({
+                amount:amount,
+                currency:"usd",
+                payment_method_types:["card"]
+            });
+            res.send({
+                clientSecret:paymentIntent.client_secret
+            })
         })
 
         // Send a ping to confirm a successful connection
